8b2d3188fa853c65de6904b32ccfbac7aecd13a4
kamil <kamil@user.com>
2021-01-23 23:04:38 +0100

    added role dropdown

--- users/forms.py
+++ users/forms.py
@@ -15,10 +15,16 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+GEEKS_CHOICES =( 
+    ("1", "Wolontariusz"), 
+    ("2", "Uczestnik"), 
+    ("3", "Organizator"), 
+)
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-
+    rodzaj_użytkownika = forms.ChoiceField(choices = GEEKS_CHOICES)
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+    
