e3c8009be917181a8597b1017871e8083bfcb215
kamil <kamil@user.com>
2021-01-25 04:34:52 +0100

    added bootstrap integration

--- confexplore/settings.py
+++ confexplore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'bootstrap4',
     
     # user created apps
     'content.apps.ContentConfig',
